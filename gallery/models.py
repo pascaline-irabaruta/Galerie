534a81892dad7e06861676130cef6097b5ec6a33
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-12-21 21:51:08 +0200

    created a category model

--- gallery/models.py
+++ gallery/models.py
@@ -24,3 +24,21 @@ class Location(models.Model):
     @classmethod
     def find_photos_by_location(cls, id):
         return cls.objects.filter(photo__location__id = id)
+
+class Category(models.Model):
+    """
+    A class for the category the Photo falls under
+    """
+    name = models.CharField(max_length=144)
+
+    def __str__(self):
+        """
+        String representation
+        """
+        return self.name
+
+    def save_category(self):
+        """
+        A method to save the category name
+        """
+        return self.save()
