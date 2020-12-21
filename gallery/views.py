2e3c07256dcc918a011c7025de48c7b5088443e5
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-12-21 22:56:05 +0200

    created index view method

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from gallery.models import Photo, Location
 
 # Create your views here.
+def index(request):
+    photos = Photo.show_all_photos()
+    return render(request, "gallery/index.html", context={"photos":photos})
