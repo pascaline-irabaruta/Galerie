e15910554b3045918dea0736fd1c4c7d9cf0987e
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-12-21 23:00:23 +0200

    created a search view method which renders search.html

--- gallery/views.py
+++ gallery/views.py
@@ -5,3 +5,18 @@ from gallery.models import Photo, Location
 def index(request):
     photos = Photo.show_all_photos()
     return render(request, "gallery/index.html", context={"photos":photos})
+
+def search(request):
+
+    if request.method == "GET":
+        search_term = request.GET.get("search")
+        searched_photos = Photo.search_photo_by_category(search_term)
+        results = len(searched_photos)
+        message = "{}".format(search_term)
+
+        return render(request, "gallery/search.html", context={"message":message,
+                                                               "photos":searched_photos,
+                                                               "results":results})
+    else:
+        message = "You have not searched for any photo"
+        return render(request, "gallery/search.html", context={"message":message})
